0aa8c6eeb8bd8eacce91c62312d13ada2a2044ee
Florian Grazina <Floriangrazina@gmail.com>
2025-01-04 23:35:28 +0100

    Scriptable object

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Quizz Question", fileName ="New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2, 6)]
+    [SerializeField] protected string question = "Enter new question text here";
+
+}
