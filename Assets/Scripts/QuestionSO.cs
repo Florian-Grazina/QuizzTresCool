49d7990001c404148aed8db1d7396426f2eb65ce
Florian Grazina <Floriangrazina@gmail.com>
2025-01-05 17:10:51 +0100

    add answers

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -6,6 +6,12 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] protected string question = "Enter new question text here";
+    [SerializeField] protected string[] answers = new string [4];
+    [SerializeField] protected int correctAnswerIndex;
 
+    #region public methods
     public string GetQuestion() => question;
+    public string GetAnswer(int index) => answers[index];
+    public int GetCorrectAnswerIndex() => correctAnswerIndex;
+    #endregion
 }
