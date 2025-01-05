878fd6f8b15449ffa525af3029b074334f562072
Florian Grazina <Floriangrazina@gmail.com>
2025-01-05 17:24:04 +0100

    add answers to button

--- Assets/Scripts/Quizz.cs
+++ Assets/Scripts/Quizz.cs
@@ -5,9 +5,16 @@ public class Quizz : MonoBehaviour
 {
     [SerializeField] protected TextMeshProUGUI questionText;
     [SerializeField] protected QuestionSO question;
+    [SerializeField] protected GameObject[] answerButtons;
 
     protected void Start()
     {
         questionText.text = question.GetQuestion();
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
     }
 }
