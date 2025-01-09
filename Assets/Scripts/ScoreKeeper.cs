64b90fd4cb40fed4bb7cf5ea27a4b3f7c69cafc1
Florian Grazina <Floriangrazina@gmail.com>
2025-01-09 21:31:05 +0100

    create score keeper

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    protected int correctAnswers = 0;
+    protected int questionsSeen = 0;
+
+    protected void Start()
+    {
+        
+    }
+
+    protected void Update()
+    {
+        
+    }
+}
